10a03513b06c960a350f9275337696662006dadd
rascho1 <raphael.schoeni@he-arc.ch>
2019-04-08 21:01:15 +0200

    Debut implementation class métier Participant

--- src/main/java/ch/hearc/odi/koulutus/business/Participant.java
+++ src/main/java/ch/hearc/odi/koulutus/business/Participant.java
@@ -1,5 +1,30 @@
 package ch.hearc.odi.koulutus.business;
 
-public class Participant {
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Participant {
+  private Integer id;
+  private String firstName;
+  private String LastName;
+  private Date birthdate;
+  private List<Course> courses;
+
+  public Participant(){courses = new ArrayList<>(); }
+
+  public Participant(String firstName, String lastName, Date birthdate) {
+    this();
+    this.firstName = firstName;
+    this.LastName = lastName;
+    this.birthdate = birthdate;
+  }
+
+  public Participant(Integer id, String firstName, String lastName, Date birthdate) {
+    this.id = id;
+    this.firstName = firstName;
+    LastName = lastName;
+    this.birthdate = birthdate;
+  }
 }
+
