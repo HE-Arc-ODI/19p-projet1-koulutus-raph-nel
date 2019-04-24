4e6ce45f26cea3afe1f4fc9721d9e1b8c8a7c035
rascho1 <raphael.schoeni@he-arc.ch>
2019-04-24 12:05:05 +0200

    Class business; Session

---
+++ src/main/java/ch/hearc/odi/koulutus/business/Session.java
@@ -0,0 +1,72 @@
+package ch.hearc.odi.koulutus.business;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table (name = "Session")
+public class Session {
+  private Long  id;
+  private Date startDateTime;
+  private Date endDateTime;
+  private Double price;
+  private String room;
+
+  public Session() {
+  }
+
+  public Session(Long id, Date startDateTime, Date endDateTime, Double price, String room) {
+    this.id = id;
+    this.startDateTime = startDateTime;
+    this.endDateTime = endDateTime;
+    this.price = price;
+    this.room = room;
+  }
+     @Id
+     @GeneratedValue(generator = "increment")
+     @GenericGenerator(name = "increment", strategy = "increment")
+    public Long getId() {
+      return id;
+    }
+
+    public void setId(Long id) {
+      this.id = id;
+    }
+
+    public Date getStartDateTime() {
+      return startDateTime;
+    }
+
+    public void setStartDateTime(Date startDateTime) {
+      this.startDateTime = startDateTime;
+    }
+
+    public Date getEndDateTime() {
+      return endDateTime;
+    }
+
+    public void setEndDateTime(Date endDateTime) {
+      this.endDateTime = endDateTime;
+    }
+
+    public Double getPrice() {
+      return price;
+    }
+
+    public void setPrice(Double price) {
+      this.price = price;
+    }
+
+    public String getRoom() {
+      return room;
+    }
+
+    public void setRoom(String room) {
+      this.room = room;
+    }
+  }
+
