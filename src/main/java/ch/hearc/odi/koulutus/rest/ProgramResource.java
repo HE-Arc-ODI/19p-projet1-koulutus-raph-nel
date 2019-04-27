ceae36af8448bdef4654de72fa22df22f6cd0b6f
rascho1 <raphael.schoeni@he-arc.ch>
2019-04-27 16:40:31 +0200

    rest/ProgramResource; implement updateProgram

--- src/main/java/ch/hearc/odi/koulutus/rest/ProgramResource.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/ProgramResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -40,5 +41,11 @@ public class ProgramResource {
   @Path("{programId}")
   public void deletProgram(@PathParam("programId") long programId){
      persistenceService.deleteProgram(programId);
+  }
+
+  @PUT
+  @Path("{programId}")
+  public Program updateProgram(@PathParam("programId") Long programId, Program program){
+    return persistenceService.updateProgram(programId,program.getName(),program.getRichDescription(),program.getField(),program.getPrice());
   }
 }
