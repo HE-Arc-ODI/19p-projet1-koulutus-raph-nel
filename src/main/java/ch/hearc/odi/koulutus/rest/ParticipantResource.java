a5aa6a90e6c4d2dfb22739440a8d152b8daae6aa
rascho1 <raphael.schoeni@he-arc.ch>
2019-04-28 22:59:19 +0200

    ParticipantResource; implement getParticipants

--- src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
+++ src/main/java/ch/hearc/odi/koulutus/rest/ParticipantResource.java
@@ -1,8 +1,11 @@
 package ch.hearc.odi.koulutus.rest;
 
+import ch.hearc.odi.koulutus.business.Participant;
 import ch.hearc.odi.koulutus.services.PersistenceService;
+import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -14,6 +17,11 @@ public class ParticipantResource {
 
   @Inject
   private PersistenceService persistenceService;
+
+  @GET
+  public List<Participant> getParticipants(){
+    return persistenceService.getParticipants();
+  }
 
 
 }
